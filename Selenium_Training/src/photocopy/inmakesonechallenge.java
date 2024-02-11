3245e231bcb53c2cce7a71f268ed8096cec2bba8
Jawahar98It <jawaharsumathi23@gmail.com>
2024-02-11 15:08:33 +0530

    screenshot automaton

---
+++ Selenium_Training/src/photocopy/inmakesonechallenge.java
@@ -0,0 +1,30 @@
+package photocopy;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class inmakesonechallenge {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		System.out.println("Challenge 1");
+		System.setProperty("webdriver.chrome.driver", "E:\\Training_Selenium\\Selenium_Training\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://lh.inmakesedu.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(600);
+		TakesScreenshot ts = (TakesScreenshot)driver;
+		File image=ts.getScreenshotAs(OutputType.FILE);
+		File f= new File("E:\\Training_Selenium\\Selenium_Training\\pagescreenshots\\inmakes.png");
+		FileUtils.copyFile(image, f);
+		driver.close();
+		driver.quit();
+
+	}
+
+}
